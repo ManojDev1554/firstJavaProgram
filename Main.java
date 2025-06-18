19961040574bda2090f2f0b8aa43d1de0e77d32c
ManojDev1554 <mkchandora1554@gmail.com>
2025-06-18 22:17:58 +0530

    Add files via upload
    the actual file with main code

---
+++ Main.java
@@ -0,0 +1,17 @@
+package com.devManoj;
+
+// first hello world program code with package making command
+// in cmd compile like this : javac -d . Main.java
+// run like this : java com.devManoj.Main
+
+public class Main
+{
+	
+	//main method the entry point
+	
+	public static void main(String... mk)
+	{
+		// hello World! prints on the console
+		System.out.println("hello World!");
+	}
+}
